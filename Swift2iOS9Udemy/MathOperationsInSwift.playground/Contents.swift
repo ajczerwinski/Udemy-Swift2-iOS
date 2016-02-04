80882a6060e4271d73f795debc7438a0d1eca3b4
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-03 19:13:31 -0800

    first commit

---
+++ Swift2iOS9Udemy/MathOperationsInSwift.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var variable1 = 15
+var variable2 = 5
+
+var sum = variable1 + variable2
+
+var multiplier = 9 * 5
+
+var difference = variable1 - variable2
+
+var dividedResult = variable1 / variable2
+
+var one = 1, two = "2", three = 3.0
+
+let four = "Four", five = 5.0, six = 6
+
+var explicitDouble: Double
+
+var explicitInteger: Int = 423
+
+var stringVar: String = "Hello"
+
+var anotherStringVar = "World"
+
+var helloWorld = stringVar + anotherStringVar
+
+helloWorld = "\(stringVar) " + "\(anotherStringVar)"
+
---
+++ Swift2iOS9Udemy/Variables.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+print(str)
+
+var number = 20
+
+var decimal = 50.5
+
+decimal = 20.23
+
+print(decimal)
+
+let constant = 20
+
+var account1 = 0.0, account2 = 1.1, account3 = 3.6
+
+print(account3)
+
+var newMessage: String
+newMessage = "Hi, this is a new message!"
+
+var red, green, blue: Double
+
+
+
+
+
