aff1e3ece05761b466bb454b8a05e3337a2a3522
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-04 18:00:11 -0800

    simple function practice exercise

---
+++ Swift2iOS9Udemy/Exercise Functions.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+func add(paramA: Double, paramB: Double) -> Double {
+    let sum = paramA + paramB
+    return sum
+}
+
+func subtract(paramA: Int, paramB: Int) -> Int {
+    let difference = paramB - paramA
+    return difference
+}
+
+func multiply(paramA: Float, paramB: Float) -> Float {
+    let product = paramA * paramB
+    return product
+}
+
+func divide(paramA: Double, paramB: Double) -> Double {
+    let divisor = paramA / paramB
+    return divisor
+}
+
+add(4.0, paramB: 7.3)
+
+subtract(9, paramB: 20)
+
+multiply(9, paramB: 22)
+
+divide(33, paramB: 11)
+
