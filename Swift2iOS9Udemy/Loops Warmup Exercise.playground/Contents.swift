ea7c72830b2c035676b945730c3d1d1c7c1b34b4
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-08 20:03:31 -0800

    add booya (fizzbuzz) exercise

---
+++ Swift2iOS9Udemy/Loops Warmup Exercise.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+func booYa() {
+    for var i = 1; i < 201; i++ {
+        if i % 15 == 0 {
+            print("BooYa")
+        } else if
+            i % 5 == 0 {
+                print("Ya")
+            } else if
+                i % 3 == 0 {
+                    print("Boo")
+            }
+        }
+    }
+
+booYa()
