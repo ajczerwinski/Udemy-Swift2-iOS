4eeeb3e10a2e47fab1914e2c1915c0aaed3cade7
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-10 23:20:37 -0800

    add v1 of click and drag functionality to little monster app

--- Swift2iOS9Udemy/My Little Monster Practice/My Little Monster Practice/ViewController.swift
+++ Swift2iOS9Udemy/My Little Monster Practice/My Little Monster Practice/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,9 @@ class ViewController: UIViewController {
         
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("I am touching the touch screen")
+    }
 
 }
 
