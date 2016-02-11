6cccb323c6ba1bfc705ef8c036909a8dfc523486
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-10 17:58:37 -0800

    add sound effects and game music for rpg oop game

--- Swift2iOS9Udemy/RPG OOP Game Assignment/RPG OOP Game Assignment/ViewController.swift
+++ Swift2iOS9Udemy/RPG OOP Game Assignment/RPG OOP Game Assignment/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -21,9 +22,43 @@ class ViewController: UIViewController {
 
     var player1: Player!
     var player2: Player!
+    var gameMusic: AVAudioPlayer!
+    var attackSound: AVAudioPlayer!
+    var deathSound: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let path1 = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")
+        let soundURL1 = NSURL(fileURLWithPath: path1!)
+        
+        do {
+            try gameMusic = AVAudioPlayer(contentsOfURL: soundURL1)
+            gameMusic.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        let path2 = NSBundle.mainBundle().pathForResource("sword", ofType: "wav")
+        let soundURL2 = NSURL(fileURLWithPath: path2!)
+        
+        do {
+            try attackSound = AVAudioPlayer(contentsOfURL: soundURL2)
+            attackSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        let path3 = NSBundle.mainBundle().pathForResource("death", ofType: "wav")
+        let soundURL3 = NSURL(fileURLWithPath: path3!)
+        
+        do {
+            try deathSound = AVAudioPlayer(contentsOfURL: soundURL3)
+            deathSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        musicPlayer()
         player1 = Player(name: "AwesomeGuyLolz", hp: 150, attackPwr: 10)
         player2 = Player(name: "StronkFightzor", hp: 90, attackPwr: 20)
         gameTextOutlet.text = "Welcome to the OOP RPG!"
@@ -36,6 +71,7 @@ class ViewController: UIViewController {
     @IBAction func player1AttackButtonPressed(sender: AnyObject) {
         
         player2.attemptAttack(player1.attackPwr)
+        swordSound()
         disablePlayer2AttackButton()
         gameTextOutlet.text = "\(player1.name) attacks \(player2.name) for \(player1.attackPwr) damage!"
         
@@ -43,6 +79,8 @@ class ViewController: UIViewController {
         if !player2.isAlive {
             hidePlayer2AttackButton()
             gameTextOutlet.text = "\(player1.name) won!"
+            stopMusic()
+            deathSound.play()
             showResetButton()
         }
         
@@ -51,6 +89,7 @@ class ViewController: UIViewController {
     @IBAction func player2AttackButtonPressed(sender: AnyObject) {
         
         player1.attemptAttack(player2.attackPwr)
+        swordSound()
         disablePlayer1AttackButton()
         gameTextOutlet.text = "\(player2.name) attacks \(player1.name) for \(player2.attackPwr) damage!"
         
@@ -58,6 +97,8 @@ class ViewController: UIViewController {
         if !player1.isAlive {
             hidePlayer1AttackButton()
             gameTextOutlet.text = "\(player2.name) won!"
+            stopMusic()
+            deathSound.play()
             showResetButton()
         }
         
@@ -105,7 +146,20 @@ class ViewController: UIViewController {
         self.viewDidLoad()
     }
     
+    func musicPlayer() {
+        gameMusic.play()
+    }
     
+    func stopMusic() {
+        gameMusic.stop()
+    }
+    
+    func swordSound() {
+        if attackSound.playing {
+            attackSound.stop()
+        }
+        attackSound.play()
+    }
         
 
 }
