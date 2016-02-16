62061d2d7b83ad9802cc46366d7a894fe547f716
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-15 19:50:21 -0800

    add About Page of app, about data and preliminary layout

---
+++ Swift2iOS9Udemy/My Favorite Places - Thompson Falls/My Favorite Places - Thompson Falls/AboutVC.swift
@@ -0,0 +1,24 @@
+//
+//  AboutVC.swift
+//  My Favorite Places - Thompson Falls
+//
+//  Created by Allen Czerwinski on 2/15/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
