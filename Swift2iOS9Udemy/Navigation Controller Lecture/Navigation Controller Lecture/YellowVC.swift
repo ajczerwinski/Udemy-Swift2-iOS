f997c0e9593c0b19a04b023864a92b02de674a59
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-16 19:26:58 -0800

    add nav view controller lecture

---
+++ Swift2iOS9Udemy/Navigation Controller Lecture/Navigation Controller Lecture/YellowVC.swift
@@ -0,0 +1,38 @@
+//
+//  YellowVC.swift
+//  Navigation Controller Lecture
+//
+//  Created by Allen Czerwinski on 2/16/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func popVC(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+}
