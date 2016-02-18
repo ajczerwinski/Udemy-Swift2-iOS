76d63ebdd03fa8fc060e051add729beeca23b022
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-18 15:51:42 -0800

    create add and detail view controllers and detail VC UI

---
+++ Swift2iOS9Udemy/Favorite Movies/Favorite Movies/DetailsVC.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsVC.swift
+//  Favorite Movies
+//
+//  Created by Allen Czerwinski on 2/18/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
