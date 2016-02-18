d7b9b86a237fd29d28f483f6686c7a996400a94d
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-18 15:23:52 -0800

    add basic add-image UI to Favorite Movies app and final parts of recipez app

---
+++ Swift2iOS9Udemy/Favorite Movies/Favorite Movies/AddMovieVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddMovieVC.swift
+//  Favorite Movies
+//
+//  Created by Allen Czerwinski on 2/18/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
