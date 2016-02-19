edf78e4088641ba4f577d57d6770a4290d8680cb
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-18 19:57:34 -0800

    create UI for details view controller

--- Swift2iOS9Udemy/Favorite Movies/Favorite Movies/DetailsVC.swift
+++ Swift2iOS9Udemy/Favorite Movies/Favorite Movies/DetailsVC.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class DetailsVC: UIViewController {
+    
+    @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDesc: UILabel!
+    @IBOutlet weak var imdbURL: UILabel!
+    @IBOutlet weak var imdbPlot: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        func configureCell(movie: Movie) {
+            movieTitle.text = movie.title
+            movieDesc.text = movie.movieDescription
+            movieImg.image = movie.getMovieImg()
+            imdbURL.text = movie.link
+            imdbPlot.text = movie.plot
+        }
     }
     
 }
