2348061f58821eae9f612e98c5d2fedba439e0a2
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-19 16:21:58 -0800

    add location authorization code

--- Swift2iOS9Udemy/DevBootcamps/DevBootcamps/LocationVC.swift
+++ Swift2iOS9Udemy/DevBootcamps/DevBootcamps/LocationVC.swift
@@ -9,20 +9,29 @@
 import UIKit
 import MapKit
 
-class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var map: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
 
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -35,7 +44,13 @@ class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
     }
 
-    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 
 
 }
