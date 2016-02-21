81f775c629f38ef4d9614420ef92299b1da53d16
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-21 15:44:27 -0800

    add custom view PokeCell

---
+++ Swift2iOS9Udemy/Pokedex/Pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Allen Czerwinski on 2/21/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
