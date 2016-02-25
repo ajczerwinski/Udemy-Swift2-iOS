0b4681de1756aa0f8b9107402f027706adbe4967
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-24 23:53:57 -0800

    install Firebase cocoapods and Facebook authentication into Xcode project

---
+++ Swift2iOS9Udemy/Udemy App Showcase/Pods/Target Support Files/Pods-Udemy App Showcase/Pods-Udemy App Showcase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Udemy_App_Showcase : NSObject
+@end
+@implementation PodsDummy_Pods_Udemy_App_Showcase
+@end
---
+++ Swift2iOS9Udemy/Udemy App Showcase/Pods/Target Support Files/Pods-Udemy App Showcase/Pods-Udemy App Showcase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Udemy_App_ShowcaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Udemy_App_ShowcaseVersionString[];
+
