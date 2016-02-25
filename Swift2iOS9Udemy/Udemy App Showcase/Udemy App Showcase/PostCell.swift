14f3edba335040690e53938d0291c098aab1e12b
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-25 15:26:54 -0800

    implement custom cell basic UI

---
+++ Swift2iOS9Udemy/Udemy App Showcase/Udemy App Showcase/PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  Udemy App Showcase
+//
+//  Created by Allen Czerwinski on 2/25/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
