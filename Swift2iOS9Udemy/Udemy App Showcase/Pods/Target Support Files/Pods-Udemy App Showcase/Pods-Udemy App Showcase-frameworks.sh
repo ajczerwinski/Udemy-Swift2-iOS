3025108f223bc4163635f80e01d2314bffff3ce7
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-25 19:28:36 -0800

    implement image caching and image rendering in UI

--- Swift2iOS9Udemy/Udemy App Showcase/Pods/Target Support Files/Pods-Udemy App Showcase/Pods-Udemy App Showcase-frameworks.sh
+++ Swift2iOS9Udemy/Udemy App Showcase/Pods/Target Support Files/Pods-Udemy App Showcase/Pods-Udemy App Showcase-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Udemy App Showcase/Alamofire.framework"
   install_framework "Pods-Udemy App Showcase/Firebase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Udemy App Showcase/Alamofire.framework"
   install_framework "Pods-Udemy App Showcase/Firebase.framework"
 fi
