003504294d768578befc7c1137741865c62544f1
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-04-27 22:30:19 -0700

    feat: complete ObjC string exercise StringFling

--- ObjcCrashCourse/StringFling/StringFling/ViewController.m
+++ ObjcCrashCourse/StringFling/StringFling/ViewController.m
@@ -21,7 +21,42 @@
     NSString *allocatedString = [[NSString alloc]init];
     
     NSLog(@"Name: %@",firstName);
+    
+    NSString *fullName = [NSString stringWithFormat:@"%@ Smith %@", firstName, @"Charles the III"];
+    
+    NSLog(@"Full Name: %@",fullName);
+    
+    NSString *display = [fullName stringByAppendingString:@" - died in 1448"];
+    
+    NSLog(@"%@", display);
+    
+    NSString *var1 = @"Junk";
+    NSString *var2 = @"in the trunk";
+    NSString *var3 = @"junk";
+    
+    // Don't compare strings this way
+//    if (var1 != var2) {
+//        NSLog(@"They are not the same!");
+//    }
+    
+    // Compare strings this way instead (using the built in method)
+    if ([var1 isEqualToString:var2]) {
+        NSLog(@"They are the same!");
+    } else {
+        NSLog(@"They are not the same yo!");
+    }
 
+    if ([var1.lowercaseString isEqualToString:var3]) {
+        NSLog(@"They are the same");
+    } else {
+        NSLog(@"They are not the same");
+    }
+    
+    if ([var1 caseInsensitiveCompare:var3] == NSOrderedSame) {
+        NSLog(@"They AREREREE the same");
+    } else {
+        NSLog(@"They are NOTTTT the same");
+    }
 }
 
 - (void)didReceiveMemoryWarning {
