102ae0ab6ccad591e68abe27448473b46f616cca
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-01 23:22:34 -0700

    feat: add InMyHoodObjC exercise

---
+++ ObjcCrashCourse/InMyHoodObjC/InMyHoodObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InMyHoodObjC
+//
+//  Created by Allen Czerwinski on 5/1/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcCrashCourse/InMyHoodObjC/InMyHoodObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InMyHoodObjC
+//
+//  Created by Allen Czerwinski on 5/1/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
