a64954865fe0a91edf920d39fbbe7d14c9a242b5
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-10 15:46:39 -0700

    add getTutorials method to app to use NSURLSession to access the node server

--- ObjcCrashCourse/DevslopesTutorial/DevslopesTutorial/HTTPService.m
+++ ObjcCrashCourse/DevslopesTutorial/DevslopesTutorial/HTTPService.m
@@ -8,6 +8,9 @@
 
 #import "HTTPService.h"
 
+#define URL_BASE "http://localhost:8000"
+#define URL_TUTORIALS "/tutorials"
+
 @implementation HTTPService
 
 
@@ -24,6 +27,27 @@
     
     return sharedInstance;
     
+}
+
+-(void) getTutorials {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%s%s", URL_BASE, URL_TUTORIALS]];
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (data != nil) {
+            NSError *err;
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
+            
+            if (err == nil) {
+                
+                NSLog(@"JSON: %@",json.debugDescription);
+                
+            } else {
+                NSLog(@"Err: %@",error.debugDescription);
+            }
+        }
+        
+    }] resume];
 }
 
 @end
