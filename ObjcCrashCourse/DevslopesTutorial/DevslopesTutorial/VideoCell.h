3c6cb3e39f5b794455aca3e971c9e7f09e4d876e
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-12 16:20:14 -0700

    feat: add VideoCell class

---
+++ ObjcCrashCourse/DevslopesTutorial/DevslopesTutorial/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  DevslopesTutorial
+//
+//  Created by Allen Czerwinski on 5/12/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
