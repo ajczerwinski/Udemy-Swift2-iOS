cb85016c2a045cbea42846ef1b8e3385b024b192
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-14 13:33:20 -0700

    feat: add VideoVC to storyboard and connect it via prepareForSegue and performSegueWithIdentifier

---
+++ ObjcCrashCourse/DevslopesTutorial/DevslopesTutorial/VideoVC.h
@@ -0,0 +1,14 @@
+//
+//  VideoVC.h
+//  DevslopesTutorial
+//
+//  Created by Allen Czerwinski on 5/14/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+#import "ViewController.h"
+@class Video;
+
+@interface VideoVC : ViewController
+@property(nonatomic,strong) Video *video;
+@end
---
+++ ObjcCrashCourse/DevslopesTutorial/DevslopesTutorial/VideoVC.m
@@ -0,0 +1,47 @@
+//
+//  VideoVC.m
+//  DevslopesTutorial
+//
+//  Created by Allen Czerwinski on 5/14/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+#import "VideoVC.h"
+#import "Video.h"
+
+@interface VideoVC ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLbl;
+@property (weak, nonatomic) IBOutlet UILabel *descLbl;
+
+@end
+
+@implementation VideoVC
+- (IBAction)donePressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.titleLbl.text = self.video.videoTitle;
+    self.descLbl.text = self.video.videoDescription;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
