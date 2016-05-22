4f2d73832c7c5aebcd024a5471d7d2de541c7856
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-22 00:08:18 -0700

    feat: use Facebook Pop to animate email/password/login, add Util and AnimateEngine

---
+++ IntermediateiOS/BrainTeaser/BrainTeaser/AnimationEngine.swift
@@ -0,0 +1,10 @@
+//
+//  AnimationEngine.swift
+//  BrainTeaser
+//
+//  Created by Allen Czerwinski on 5/22/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
--- IntermediateiOS/BrainTeaser/BrainTeaser/LoginVC.swift
+++ IntermediateiOS/BrainTeaser/BrainTeaser/LoginVC.swift
@@ -11,14 +11,29 @@ import pop
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var emailConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var passwordConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var loginConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(4) { () -> Void in
+            self.emailConstraint.constant = -100
+            self.view.layoutIfNeeded()
+        }
     }
 
 
