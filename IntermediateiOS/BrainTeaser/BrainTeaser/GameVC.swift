df76179eae2389bd89bade746c3a1c9e4e4dc466
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-23 21:06:31 -0700

    feat: add GameVC

---
+++ IntermediateiOS/BrainTeaser/BrainTeaser/GameVC.swift
@@ -0,0 +1,19 @@
+//
+//  GameVC.swift
+//  BrainTeaser
+//
+//  Created by Allen Czerwinski on 5/23/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
