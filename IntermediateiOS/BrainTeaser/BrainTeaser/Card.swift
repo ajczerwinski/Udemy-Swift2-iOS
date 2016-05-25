a42b89afd21580ccf86dd9da215885f31e2f3171
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-24 18:37:07 -0700

    feat: finish configuring Card.xib

--- IntermediateiOS/BrainTeaser/BrainTeaser/Card.swift
+++ IntermediateiOS/BrainTeaser/BrainTeaser/Card.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class Card: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    let shapes = ["blue_ball", "green_square", "red_triangle"]
+    
+    var currentShape: String!
+    
+    @IBOutlet weak var shapeImage: UIImageView!
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            setupView()
+        }
     }
-    */
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    override func awakeFromNib() {
+        setupView()
+        selectShape()
+    }
+    
+    func setupView() {
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSizeMake(0.0, 2.0)
+        self.layer.shadowColor = UIColor(red: 157.0/255.0, green: 157.0/255.0, blue: 157.0/255.0, alpha: 1.0).CGColor
+        self.setNeedsLayout()
+    }
+    
+    func selectShape() {
+        currentShape = shapes[Int(arc4random_uniform(3))]
+        shapeImage.image = UIImage(named: currentShape)
+    }
 
 }
