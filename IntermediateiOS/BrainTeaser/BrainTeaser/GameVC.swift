7ad0301c47a8dd73387bceea953a4840cd88058c
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-25 21:59:07 -0700

    fix: add self.layer.cornerRadius to setupView so cornerRadius displays properly in card.swift

--- IntermediateiOS/BrainTeaser/BrainTeaser/GameVC.swift
+++ IntermediateiOS/BrainTeaser/BrainTeaser/GameVC.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import pop
 
 class GameVC: UIViewController {
 
+    @IBOutlet weak var yesBtn: CustomButton!
+    @IBOutlet weak var noBtn: CustomButton!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    var currentCard: Card!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentCard = NSBundle.mainBundle().loadNibNamed("Card", owner: self, options: nil)[0] as? Card
+        currentCard.center = AnimationEngine.screenCenterPosition
+        self.view.addSubview(currentCard)
 
-        // Do any additional setup after loading the view.
     }
 
 }
