497ffd7fa2443c400a083ae408c9f2c837bec870
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-06-04 16:50:22 -0700

    feat: add PlayerVC, connect to storyboard, and configure UITableViewCell

---
+++ IntermediateiOS/PlayerScoop/PlayerScoop/PlayerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlayerViewController.swift
+//  PlayerScoop
+//
+//  Created by Allen Czerwinski on 6/4/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var players = Player.createPlayers()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.players.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let player = self.players[indexPath.row]
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = player.name
+        
+        return cell
+    }
+
+}
