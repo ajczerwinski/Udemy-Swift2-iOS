3d43f0efbd98d650daf9dbd91ff09f59f921024e
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-06-03 17:37:32 -0700

    feat: populate more player data

--- IntermediateiOS/PlayerScoop/PlayerScoop/Player.swift
+++ IntermediateiOS/PlayerScoop/PlayerScoop/Player.swift
@@ -20,6 +20,17 @@ struct Player {
     
     static func createPlayers() -> [Player] {
         let rodman = Player(name: "Dennis Rodman", age: 55, college: "Southeastern Oklahoma State", height: "6' 7\"", image: UIImage(named: "rodman.jpg")!, backgroundColor: UIColor.redColor(), textColor1: UIColor.purpleColor(), textColor2: UIColor.whiteColor())
-        return [rodman]
+        
+        let barkley = Player(name: "Charles Barkley", age: 53, college: "Auburn", height: "6' 6\"", image: UIImage(named: "barkley.jpg")!, backgroundColor: UIColor.blackColor(), textColor1: UIColor.blackColor(), textColor2: UIColor.blackColor())
+        
+        let curry = Player(name: "Steph Curry", age: 28, college: "Davidson", height: "6' 3\"", image: UIImage(named: "curry.jpg")!, backgroundColor: UIColor.blackColor(), textColor1: UIColor.blackColor(), textColor2: UIColor.blackColor()   )
+        
+        let nash = Player(name: "Steve Nash", age: 42, college: "Santa Clara", height: "6' 3\"", image: UIImage(named: "nash.jpg")!, backgroundColor: UIColor.blackColor(), textColor1: UIColor.blackColor(), textColor2: UIColor.blackColor())
+        
+        let laimbeer = Player(name: "Bill Laimbeer", age: 59, college: "Notre Dame", height: "6' 11\"", image: UIImage(named: "laimbeer.jpg")!, backgroundColor: UIColor.blackColor(), textColor1: UIColor.blackColor(), textColor2: UIColor.blackColor())
+        
+        let shaq = Player(name: "Shaquille O'Neil", age: 44, college: "Louisiana State University", height: "7' 1\"", image: UIImage(named: "shaq.jpg")!, backgroundColor: UIColor.blackColor(), textColor1: UIColor.blackColor(), textColor2: UIColor.blackColor())
+        
+        return [rodman, barkley, curry, nash, laimbeer, shaq]
     }
 }
