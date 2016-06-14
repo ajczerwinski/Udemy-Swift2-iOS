35180727979451d8de83030b5c17cbc3430b8ee0
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-06-13 17:12:30 -0700

    feat: create project for FoodShack app to practice programmatically creating UIStoryboard

---
+++ IntermediateiOS/FoodShack/FoodShack/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FoodShack
+//
+//  Created by Allen Czerwinski on 6/13/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let burgerBtn = UIButton()
+    let pizzaBtn = UIButton()
+    let sandwichBtn = UIButton()
+    let tacoBtn = UIButton()
+    let orderNowBtn = UIButton()
+    let selectedItemThumbImg = UIImageView(image: UIImage(named: "burger"))
+    let topViewHolder = UIView()
+    let mainThumbViewHolder = UIView()
+    let buttonViewHolder = UIView()
+    let bottomViewHolder = UIView()
+    let topThumbBtn = UIButton()
+    let topTitleLbl = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
