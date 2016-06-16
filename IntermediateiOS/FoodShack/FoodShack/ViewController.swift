9c73625099cf8fa49a2eb9a91b562d49bfa92102
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-06-16 15:12:08 -0700

    feat: add image assets and implement burger button in top left

--- IntermediateiOS/FoodShack/FoodShack/ViewController.swift
+++ IntermediateiOS/FoodShack/FoodShack/ViewController.swift
@@ -35,20 +35,26 @@ class ViewController: UIViewController {
         self.buttonViewHolder.backgroundColor = UIColor(red: 242.0 / 255.0, green: 241.0 / 255.0, blue: 239.0 / 255.0, alpha: 1.0)
         self.bottomViewHolder.backgroundColor = UIColor(red: 39.0 / 255.0, green: 61.0 / 255.0, blue: 72.0 / 255.0, alpha: 1.0)
         
+        self.topThumbBtn.setImage(UIImage(named: "burger"), forState: .Normal)
+        
         self.topViewHolder.translatesAutoresizingMaskIntoConstraints = false
         self.mainThumbViewHolder.translatesAutoresizingMaskIntoConstraints = false
         self.buttonViewHolder.translatesAutoresizingMaskIntoConstraints = false
         self.bottomViewHolder.translatesAutoresizingMaskIntoConstraints = false
+        self.topThumbBtn.translatesAutoresizingMaskIntoConstraints = false
         
         self.view.addSubview(topViewHolder)
         self.view.addSubview(mainThumbViewHolder)
         self.view.addSubview(buttonViewHolder)
         self.view.addSubview(bottomViewHolder)
         
+        self.topViewHolder.addSubview(self.topThumbBtn)
+        
         self.views["topViewHolder"] = topViewHolder
         self.views["mainThumbViewHolder"] = mainThumbViewHolder
         self.views["buttonViewHolder"] = buttonViewHolder
         self.views["bottomViewHolder"] = bottomViewHolder
+        self.views["topThumbBtn"] = topThumbBtn
         
         setConstraints()
         
@@ -73,6 +79,10 @@ class ViewController: UIViewController {
         /** BOTTOM VIEW HOLDER **/
         addConstraint("V:[bottomViewHolder(50)]-|")
         addConstraint("H:|-0-[bottomViewHolder(buttonViewHolder)]-0-|")
+        
+        /** TOP THUMB BUTTON **/
+        addConstraint("V:|-5-[topThumbBtn(40)]")
+        addConstraint("H:|-5-[topThumbBtn(40)]")
         
         NSLayoutConstraint.activateConstraints(self.constraints)
         
