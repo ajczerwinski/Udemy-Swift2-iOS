5c5a1f71dea12e11385e0cbfca951b930c9a6d57
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-06-23 13:56:32 -0700

    feat: add boilerplate code to set up UITableViewDataSource and Delegate, connect UI elements to storyboard in code

---
+++ IntermediateiOS/SomeJunk/SomeJunk/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  SomeJunk
+//
+//  Created by Allen Czerwinski on 6/23/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
