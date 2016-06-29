f9025856011d8ffa78cb338a66f0404d7155555d
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-06-29 16:48:37 -0700

    feat: implement PickerView delegate and datasource code in ItemDetailsVC

--- IntermediateiOS/SomeJunk/SomeJunk/ItemDetailsVC.swift
+++ IntermediateiOS/SomeJunk/SomeJunk/ItemDetailsVC.swift
@@ -8,28 +8,38 @@
 
 import UIKit
 
-class ItemDetailsVC: UIViewController {
+class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    let stores = [Store]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
     }
-    */
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
 
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
 }
