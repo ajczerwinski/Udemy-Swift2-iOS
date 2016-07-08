bdda89451cb4ca071ecdaeb1fc991194239c4fdc
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-07-08 15:22:24 -0700

    feat: create recipe entity in Core Data and create ManagedNSModelSubclass

---
+++ IntermediateiOS/Recipez/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Recipez
+//
+//  Created by Allen Czerwinski on 7/8/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
