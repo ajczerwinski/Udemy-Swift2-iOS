a9196663ae4b05a0beea656359287105502e8ff4
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-07-08 14:23:06 -0700

    feat: add tableView delegate and datasource to VC and implement boilerplate code, connect storyboard to tableView outlet

--- IntermediateiOS/Recipez/Recipez/ViewController.swift
+++ IntermediateiOS/Recipez/Recipez/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 
 
