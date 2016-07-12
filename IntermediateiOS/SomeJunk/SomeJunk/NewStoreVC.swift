c918e4dcc7fc06e537898de633526076350cce96
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-07-12 11:13:03 -0700

    feat: create NewStoreVC and add UIVC to storyboard

---
+++ IntermediateiOS/SomeJunk/SomeJunk/NewStoreVC.swift
@@ -0,0 +1,19 @@
+//
+//  NewStoreVC.swift
+//  SomeJunk
+//
+//  Created by Allen Czerwinski on 7/12/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class NewStoreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
