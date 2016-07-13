f7b28eb2a3f1a68a46e274f9a419264d3262f6ac
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-07-13 10:29:25 -0700

    feat: connect UI elements to NewStoreVC and give user the ability to add their own store

--- IntermediateiOS/SomeJunk/SomeJunk/NewStoreVC.swift
+++ IntermediateiOS/SomeJunk/SomeJunk/NewStoreVC.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
 class NewStoreVC: UIViewController {
 
+    
+    @IBOutlet weak var storeTitle: CustomTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func addStoreBtnPressed(sender: AnyObject) {
+        
+        var store: Store!
+        
+        if storeTitle.text != "" {
+            store = NSEntityDescription.insertNewObjectForEntityForName("Store", inManagedObjectContext: ad.managedObjectContext) as! Store
+        } else {
+            print("Store title cannot be blank")
+        }
+        
+        store.name = storeTitle.text
+        
+        ad.saveContext()
+        
+        self.navigationController?.popViewControllerAnimated(true)
+        
+    }
+    
 
 }
