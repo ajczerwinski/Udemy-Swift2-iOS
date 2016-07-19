b5a0e2a88b5333de1f78f74a6dc4ddaa37526f7f
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-07-19 15:12:35 -0700

    feat: add PlayerCell and connect to tableViewCell

---
+++ IntermediateiOS/SillyValley/SillyValley/PlayerCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerCell.swift
+//  SillyValley
+//
+//  Created by Allen Czerwinski on 7/19/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    @IBOutlet weak var playerImage: UIImageView!
+    @IBOutlet weak var playerName: UILabel!
+    @IBOutlet weak var playerDesc: UILabel!
+    
+    
+    
+}
